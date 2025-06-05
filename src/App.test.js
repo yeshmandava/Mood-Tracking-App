ede8ffa81e3c9d38aa668bcfc077f56e5eadb3c0
yeshmandava <89482728+yeshmandava@users.noreply.github.com>
2025-06-04 23:32:05 -0500

    Add edit and delete options to journal entries

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders MindFlow header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByText(/MindFlow/i);
+  expect(header).toBeInTheDocument();
 });
